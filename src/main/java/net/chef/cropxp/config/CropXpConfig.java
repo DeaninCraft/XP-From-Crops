32e8712339276a2ea23dcb565b8f00ea35894a91
Chef <efrisbi1@emich.edu>
2022-04-30 17:43:38 -0400

    Switchs config to use tags instead of block states for mod compatiblility

--- src/main/java/net/chef/cropxp/config/CropXpConfig.java
+++ src/main/java/net/chef/cropxp/config/CropXpConfig.java
@@ -1,11 +1,9 @@
 package net.chef.cropxp.config;
 
 import blue.endless.jankson.Comment;
-import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.chef.cropxp.XpFromCrops;
 
 @Config(name = XpFromCrops.MOD_ID)
@@ -19,13 +17,4 @@ public class CropXpConfig implements ConfigData {
     @Comment("Amount of xp dropped from harvest : Minimum 1")
     public int amount = 1;
 
-    @ConfigEntry.Category("crop_settings")
-    @Comment("List of crops to allow xp drops")
-    public String[] crops = {
-            "minecraft:potatoes[age=7]",
-            "minecraft:carrots[age=7]",
-            "minecraft:wheat[age=7]",
-            "minecraft:beetroots[age=3]",
-            "minecraft:nether_wart[age=3]"
-    };
 }
--- src/main/java/net/chef/cropxp/events/HarvestEvent.java
+++ src/main/java/net/chef/cropxp/events/HarvestEvent.java
@@ -5,12 +5,11 @@ import net.chef.cropxp.config.ConfigRegister;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.ExperienceOrbEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.Property;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-
-import java.util.Arrays;
 
 public class HarvestEvent {
     public HarvestEvent() {
@@ -20,7 +19,7 @@ public class HarvestEvent {
             }
             try {
                 ServerWorld serverWorld = player.getServer().getWorld(world.getRegistryKey());
-                handleHarvest(serverWorld, state, pos, player);
+                handleHarvest(serverWorld, state, pos);
             }
             catch (Exception e) {
                 XpFromCrops.LOGGER.error(e.getMessage());
@@ -28,13 +27,37 @@ public class HarvestEvent {
         });
     }
 
-    public void handleHarvest(ServerWorld world, BlockState state, BlockPos pos, PlayerEntity player) {
+    public void handleHarvest(ServerWorld world, BlockState state, BlockPos pos) {
         Vec3d vec = new Vec3d(pos.getX(), pos.getY(), pos.getZ());
-        String blockString = state.toString().replace("Block{", "").replace("}", "");
-        boolean giveXp = Arrays.asList(ConfigRegister.CONFIG.crops).toString().contains(blockString);
+        boolean isMaxAge = (getCropAge(state) == getMaxCropAge(state));
+        boolean giveXp = state.isIn(BlockTags.CROPS) && isMaxAge;
         if (giveXp && world.random.nextInt(100) + 1 <= ConfigRegister.CONFIG.chance) {
             ExperienceOrbEntity.spawn(world, vec, ConfigRegister.CONFIG.amount);
             XpFromCrops.LOGGER.info(ConfigRegister.CONFIG.amount + " Experience added for " + state.getBlock().getTranslationKey());
         }
+    }
+    public static int getCropAge(BlockState state) {
+        Property AGE = null;
+        for (Property property:state.getProperties()) {
+            if(property.getName() == "age") AGE = property;
+        }
+        return Integer.parseInt(state.get(AGE).toString());
+    }
+
+    public static int getMaxCropAge(BlockState state) {
+        for (Property property:state.getProperties()) {
+            if(property.getName() == "age") {
+                Object[] ages = property.getValues().toArray();
+                double maxValue = -1;
+                int indexOfMaxValue = -1;
+                for(int i = 0; i < ages.length; i++) {
+                    if(Integer.parseInt(ages[i].toString()) > maxValue) {
+                        indexOfMaxValue = i;
+                    }
+                }
+                return Integer.parseInt(ages[indexOfMaxValue].toString());
+            }
+        }
+        return 0;
     }
 }
